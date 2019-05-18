75110493afc44feeb7836906fcc50dd1157cf7fe
pandaman64 <kointosudesuyo@infoseek.jp>
2019-05-18 19:48:10 +0900

    add runtime compatibility test

--- tests/future.rs
+++ tests/future.rs
@@ -1,4 +1,4 @@
-#![feature(generators, never_type)]
+#![feature(generators, never_type, async_await)]
 
 use eff::*;
 use futures::executor::block_on;
@@ -7,4 +7,32 @@ use futures::executor::block_on;
 fn test_future() {
     let e = lazy(|| println!("running in a future!"));
     block_on(e.into_future());
+}
+
+#[runtime::test]
+async fn test_runtime() {
+    struct Choose;
+
+    impl Effect for Choose {
+        type Output = bool;
+    }
+
+    let result = (effectful! {
+        if perform!(Choose) {
+            42
+        } else {
+            24
+        }
+    })
+    .effect::<Coproduct![Choose]>()
+    .handle(handler! {
+        x => x,
+        Choose, k => {
+            perform!(k.resume(true)) + 100
+        }
+    })
+    .into_future()
+    .await;
+
+    assert_eq!(result, 142);
 }
